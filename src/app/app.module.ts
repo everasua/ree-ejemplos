4501e4f11089a010085839d712c110001dc17f96
Adrián Suazo <adrian.suazo.lopez@everis.com>
2020-02-25 17:50:49 +0100

    Initial config

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,17 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { DatepickerModule } from '@ree/datepicker';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    DatepickerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
