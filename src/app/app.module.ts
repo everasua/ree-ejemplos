21eb3561f5ea15f35c26c9fe0b5b8e0904cefedf
javiggpecas <50137164+javiggpecas@users.noreply.github.com>
2020-02-27 09:23:40 +0100

    init forms

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AccordionModule } from '@ree/accordion';
+import { InputModule } from '@ree/input';
+import { ButtonModule } from '@ree/button';
+import { SelectorModule } from '@ree/selector';
+import { DropdownModule } from '@ree/dropdown';
+import { RadioModule } from '@ree/radio';
 import { DatepickerModule } from '@ree/datepicker';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
     AppComponent
@@ -13,6 +20,14 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     BrowserModule,
     BrowserAnimationsModule,
     AppRoutingModule,
+    AccordionModule,
+    InputModule,
+    ButtonModule,
+    SelectorModule,
+    FormsModule,
+    ReactiveFormsModule,
+    RadioModule,
+    DropdownModule,
     DatepickerModule
   ],
   providers: [],
