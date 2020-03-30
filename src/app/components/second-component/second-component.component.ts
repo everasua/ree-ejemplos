86058a674b66f2a84e01ac5523554583ee603613
Adrián Suazo <adrian.suazo.lopez@everis.com>
2020-03-30 08:58:13 +0200

    Se configura el datepicker

--- src/app/components/second-component/second-component.component.ts
+++ src/app/components/second-component/second-component.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-second-component',
@@ -7,7 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SecondComponentComponent implements OnInit {
 
-  constructor() { }
+  private formGroup: FormGroup;
+  constructor(private formBuilder: FormBuilder) {
+    this.formGroup = this.formBuilder.group({
+      datepicker: [null],
+    });
+  }
   value = null;
 
   ngOnInit() {
